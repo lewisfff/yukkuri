a2b6961eb0529c16c4229f83db5ec453227c9f1c
Lewis White <16711731@protonmail.com>
2017-11-11 17:17:27 +0000

    Something that looks like what we are going for in terms of UI

--- js/client/client.js
+++ js/client/client.js
@@ -1,2 +1,12 @@
+window.currentStep = document.querySelector('#stage span:last-of-type');
 
-console.log('game');
+console.log(currentStep);
+
+function keyCharPress(char) {
+    console.log(char, currentStep.innerText, (char == currentStep.innerText && 'MATCH'));
+    if (currentStep.innerText == char) {
+        currentStep.remove();
+    }
+}
+
+window.addEventListener('keypress', e => keyCharPress(e.key));
