5f352674fbf69c479865324ee857f25dc62b90af
lilah <lilah.willacott@gmail.com>
2017-11-11 17:02:04 +0000

    add basic koa static serve our public directory

--- js/server/server.js
+++ js/server/server.js
@@ -1,2 +1,13 @@
 
-console.log('hello, world');
+const serve = require('koa-static');
+const koa = require('koa');
+
+const pub = '../../public';
+const root = `${__dirname}/${pub}`;
+const port = process.env.PORT || 3000;
+
+const app = new koa();
+app.use(serve(root));
+app.listen(process.env.PORT || 3000);
+
+console.log(`Serving ${root} on port ${port}`);
