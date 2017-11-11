156b3f0a5bb03b15373f57b5c015e4a985ba8d13
lilah <lilah.willacott@gmail.com>
2017-11-11 19:30:26 +0000

    add socket.io to server with some boilerplate

--- js/server/server.js
+++ js/server/server.js
@@ -1,13 +1,22 @@
 
+const http = require('http');
 const serve = require('koa-static');
 const koa = require('koa');
+const socket = require('socket.io');
 
 const pub = '../../public';
 const root = `${__dirname}/${pub}`;
 const port = process.env.PORT || 3000;
 
 const app = new koa();
+const server = http.createServer(app.callback());
+const io = socket(server); 
+
 app.use(serve(root));
 app.listen(port);
 
 console.log(`Serving ${root} on port ${port}`);
+
+io.on('connection', function(socket) {
+	console.log('a user connected');
+});
